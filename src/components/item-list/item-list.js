e7ab4d850ee8703dc33d30a7565b232e50da1980
00010100 <0001101004@protonmail.com>
2018-10-15 14:53:11 +0300

    Create person details, change api person

--- src/components/item-list/item-list.js
+++ src/components/item-list/item-list.js
@@ -1,15 +1,40 @@
 import React, { Component } from 'react';
 
+import Spinner from '../spinner';
+import SwapiService from '../../services/swapi-service';
+
 import './item-list.css';
 
 export default class ItemList extends Component {
+  swapiService = new SwapiService();
+
+  state = {
+    peropleList: null,
+  };
+
+  componentDidMount() {
+    this.swapiService.getAllPeople().then((peropleList) => {
+      this.setState({ peropleList });
+    });
+  }
+
+  renderItems(arr) {
+    return arr.map(({ id, name }) => (
+      <li className="list-group-item" key={id} onClick={() => this.props.onItemSelected(id)}>
+        {name}
+      </li>
+    ));
+  }
+
   render() {
-    return (
-      <ul className="item-list list-group">
-        <li className="list-group-item">Luke Skywalker</li>
-        <li className="list-group-item">Darth Vader</li>
-        <li className="list-group-item">R2-D2</li>
-      </ul>
-    );
+    const { peropleList } = this.state;
+
+    if (!peropleList) {
+      return <Spinner />;
+    }
+
+    const items = this.renderItems(peropleList);
+
+    return <ul className="item-list list-group">{items}</ul>;
   }
 }
